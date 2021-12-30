5671332cf91c847b9c5fcac1c27dbf31b34d3718
Huy Pham <phamdinhhuy@outlook.com>
2021-12-30 11:21:43 +0700

    feature:demo reponse

--- nhanh01/demo.js
+++ nhanh01/demo.js
@@ -1,13 +1,24 @@
 const http = require('http');
+const url = require('url');
+
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req,res) => {
-    console.log(req);
+const server = http.createServer((req, res) => {
+    console.log(req.method);
+    const data={
+        'id':1,
+        'name': 'iphone'
+    };
+    
+    res.writeHead(200, {'content-type': 'text/json'});
+    res.end(JSON.stringify(data));
+
+ /*   console.log(req);
     res.statusCode= 200;
     res.setHeader('Content-type', 'text/html');
     res.end('<h2>Hello Huy</h2>');
-
+*/
 });
 
 server.listen(port,hostname, () =>{
